a70938e880c7aed0924711ebe88d40bf32e8d3de
Praveer Satyam <praveer.satyam@INpraveersatyam.local>
2019-05-14 11:59:22 +0530

    [Praveer] adds new tests in TravelTest.java

--- src/main/java/com/travel/service/PathFinder.java
+++ src/main/java/com/travel/service/PathFinder.java
@@ -1,10 +1,42 @@
 package com.travel.service;
 
+import com.travel.constants.Constants;
 import com.travel.entity.Flight;
 import com.travel.entity.Tour;
 import com.travel.util.DataInitializer;
 
+import java.util.StringTokenizer;
+
 public class PathFinder {
+
+    public boolean isAvailableInDatabase(String destination){
+        StringTokenizer stz;
+        for (String path: Constants.Paths.PATHS_ARRAY) {
+            stz = new StringTokenizer(path,",");
+            String fromDestination = stz.nextToken();
+            String toDestination = stz.nextToken();
+            if(destination.equals(fromDestination)||destination.equals(toDestination)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String checkForFlightsBetweenCities(String fromDestination, String toDestination){
+        Boolean isFromDestinationPresent = isAvailableInDatabase(fromDestination);
+        Boolean isToDestinationPresent = isAvailableInDatabase(toDestination);
+        if(isFromDestinationPresent && isToDestinationPresent){
+            return String.valueOf(isDirectFlight(fromDestination,toDestination));
+        }
+        else {
+            if(!isFromDestinationPresent){
+                return "No city named \""+fromDestination+"\" in database";
+            }
+            else{
+                return "No city named \""+toDestination+"\" in database";
+            }
+        }
+    }
     public boolean isDirectFlight(String fromDestination, String toDestination){
         Tour tour = DataInitializer.intializeTours();
         for (Flight flight: tour.getAvailableFlights()) {
--- src/test/java/com/travel/test/TravelTest.java
+++ src/test/java/com/travel/test/TravelTest.java
@@ -6,9 +6,30 @@ import static org.junit.Assert.assertEquals;
 
 public class TravelTest {
     @Test
-    public void testForTWoCities(){
+    public void testForFlightBetweenTwoCities(){
         PathFinder pathFinder = new PathFinder();
-        Boolean isFlight = pathFinder.isDirectFlight("Bangalore","Singapore");
-        assertEquals(true, isFlight);
+        String isFlightAvailableResponse = pathFinder.checkForFlightsBetweenCities("Bangalore","Singapore");
+        assertEquals("true", isFlightAvailableResponse);
+    }
+
+    @Test
+    public void testForUnavailableFlight(){
+        PathFinder pathFinder = new PathFinder();
+        String isFlightAvailableResponse = pathFinder.checkForFlightsBetweenCities("Bangalore","Tokyo");
+        assertEquals("false", isFlightAvailableResponse);
+    }
+
+    @Test
+    public void testForFlightFirstCityNotPresentInDatabase(){
+        PathFinder pathFinder = new PathFinder();
+        String isFlightAvailableResponse = pathFinder.checkForFlightsBetweenCities("Chennai","Tokyo");
+        assertEquals("No city named \"Chennai\" in database",isFlightAvailableResponse);
+    }
+
+    @Test
+    public void testForFlightSecondCityNotPresentInDatabase(){
+        PathFinder pathFinder = new PathFinder();
+        String isFlightAvailableResponse = pathFinder.checkForFlightsBetweenCities("Bangalore","Stockholm");
+        assertEquals("No city named \"Stockholm\" in database",isFlightAvailableResponse);
     }
 }
